aa82e53653ef124c7b5509bd458a6bc7d77d732e
Lamont Taylor <mlamont8@hotmail.com>
2020-07-07 08:55:45 -0400

    added model folder

--- models/user.go
+++ models/user.go
@@ -6,17 +6,3 @@ type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
-
-type TokenDetails struct {
-	AccessToken  string
-	RefreshToken string
-	AccessUuid   string
-	RefreshUuid  string
-	AtExpires    int64
-	RtExpires    int64
-}
-
-type AccessDetails struct {
-	AccessUuid string
-	UserId     uint64
-}
